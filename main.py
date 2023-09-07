e8bf3a8b871fa5950d7ceb3f6266e442116a2890
chris <chrisrossell1999@gmail.com>
2023-09-07 11:36:58 +0100

    Set up homepage for weather API

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
-@app.route("/home")
+
+@app.route("/")
 def home():
-    return render_template("demonstration.html")
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+    return render_template("home.html")
 
 
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    # df = pandas.read_csv("")
+    # temperature = df.station(date)
+    temperature = 10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
